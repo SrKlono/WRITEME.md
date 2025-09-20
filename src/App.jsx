dc0228961837f22cb12acce36624f8641fe5073a
SrKlono <danilogamasilvanb@gmail.com>
2025-09-20 13:06:04 -0300

    Markdown support added

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,40 @@
 import React from "react";
 import WindowHeader from "./components/WindowHeader";
+import Markdown from "react-markdown";
+import { useState } from "react";
 
 const App = () => {
+	const [text, setText] = useState(
+		"# Heading \n## Subheading \nHere’s a [link to example.com](https://example.com).\n Some inline code: \`\`\`javascript const x = 42;\`\`\` \n\`\`\`javascript \nfunction greet(name) { \n    return \`Hello, \${name}!\`; \n} \n\`\`\`\` \n* List item one \n* List item two \n> This is a blockquote. \n![Alt text for image](https://placehold.co/400) \nThis is **bolded text** for emphasis."
+	);
+
 	return (
 		<div className="min-h-screen flex items-center justify-center bg-grid">
 			<div className="grid grid-cols-2 gap-x-4 flex-1 px-[1rem]">
 				<div className="rounded-sm g-border bg-white w-shadow h-[calc(100dvh-2rem)] p-2">
-					<div className="flex flex-col gap-y-2  h-full">
-            <WindowHeader title={"editor.exe"}/>
+					<div className="flex flex-col gap-y-2 h-full">
+						<WindowHeader title={"editor.exe"} />
 						<textarea
-							className="text-base g-border resize-none flex-1"
-							name=""
-							id=""
+							value={text}
+							onChange={(e) => setText(e.target.value)}
+							className="text-base g-border resize-none flex-1 px-1"
+							id="editor"
+							placeholder="Type your markdown here..."
 						></textarea>
 					</div>
 				</div>
 
 				<div className="rounded-sm g-border bg-white w-shadow h-[calc(100dvh-2rem)] p-2">
-					<div className="flex flex-col gap-y-2">
-            <WindowHeader title={"previewer.exe"}/>
-          </div>
-        </div>
+					<div className="flex flex-col gap-y-2 h-full">
+						<WindowHeader title={"previewer.exe"} />
+						<div
+							id="preview"
+							className="text-base g-border resize-none flex-1 px-1 whitespace-pre-line overflow-scroll prose-sm lg:prose"
+						>
+							<Markdown>{text}</Markdown>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
